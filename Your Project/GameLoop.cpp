3dffa457d0ba8e7a1dc889d07055d3b4dc983207
RYBates <ryan.bates@students.aie.edu.au>
2015-11-04 11:47:59 -0600

    change
    trying for two players attack each other.

--- Your Project/GameLoop.cpp
+++ Your Project/GameLoop.cpp
@@ -1,7 +1,9 @@
 #include "GameLoop.h"
 #include "CustomVML.h"
-Vec2 <double> A, B, C; //Vec2 objects of data type float
-Vec3 <double> D, E, F; //Vec3 objects of data type float
+
+Vec2 <double> A, B, C; 
+Vec3 <double> D, E, F; 
+
 //cirlce a
 System::Point2D<int>pos{ 350,80 };
 System::Size2D<int>siz{ 100,50 };
@@ -9,7 +11,7 @@ System::Color<int>col{ 300,450,150,1600 };
 
 //circle b
 System::Point2D<int>po{ 800,500 };
-System::Size2D<int>si{ 350,480 };
+System::Size2D<int>si{ 350,48 };
 System::Color<int>c{ 0,255,138,255 };
 
 void GameLoop::Loop()
@@ -229,71 +231,72 @@ void GameLoop::OnKeyDown(const SDL_Keycode ac_sdlSym, const Uint16 ac_uiMod, con
 		c.Red = c.Red - 50;
 	}
 
+
+	//math library
 	if (ac_sdlSym == SDLK_1)
 	{
+	
+		cout << "2D vector A and B:" << endl;
+		A.x = 15, A.y = 35; 
+		B.x = 45, B.y = 89; 
+		A.print2(); 
+		B.print2(); 
+		cout << endl; 
+
+		cout << "2D vector addition:" << endl; 
+		C = A + B; 
+		C.print2(); 
+		cout << endl; 
+		cout << "2D vector subtraction:" << endl; 
+
+		C = A - B; 
+		C.print2(); 
+		cout << endl;
+		cout << "2D vector magnitude:" << endl; 
+		cout << "Magnitude of A is: " << A.mag2() << endl; 
+		cout << "Magnitude of B is: " << B.mag2() << endl; 
+		cout << endl; 
+
+		cout << "2D vector normalise:" << endl; 
+		cout << "Normalise vector A is: " << A.nor2X() << " " << A.nor2Y() << endl; 
+		cout << "Normalise vector B is: " << B.nor2X() << " " << B.nor2Y() << endl; 
+		cout << endl;
+		cout << "2D vector dot product:" << endl; 
+
+		C = A * B; 
+		C.dot2(); 
+		cout << endl;
+
 		
+		cout << "3D vector D and E:" << endl; 
+		D.x = 68, D.y = -54, D.z = 32; 
+		E.x = 233, E.y = -124, E.z = 43; 
+		D.print3(); 
+		E.print3(); 
+		cout << endl; 
+		cout << "3D vector addition:" << endl; 
 
-		cout << "2D vector A and B:" << endl; //info
-		A.x = 5, A.y = 4; //A = (5,4)
-		B.x = -2, B.y = 5; //B = (-2,5)
-		A.print2(); //print test
-		B.print2(); //print test
-		cout << endl; //new line
+		F = D + E; 
+		F.print3(); 
+		cout << endl;
+		cout << "3D vector subtraction:" << endl; 
 
-		cout << "2D vector addition:" << endl; //info
-		C = A + B; //C will equal ((A.x + B.x)(A.y + B.y))
-		C.print2(); //print C add value
-		cout << endl; //new line
-		cout << "2D vector subtraction:" << endl; //info
+		F = D - E; 
+		F.print3(); 
+		cout << endl; 
+		cout << "3D vector magnitude:" << endl; 
+		cout << "Magnitude of D is: " << D.mag3() << endl; 
+		cout << "Magnitude of E is: " << E.mag3() << endl; 
+		cout << endl; 
+		cout << "3D vector normalise:" << endl; 
+		cout << "Normalise vector D is: " << D.nor3X() << " " << D.nor3Y() << " " << D.nor3Z() << endl; 
+		cout << "Normalise vector E is: " << E.nor3X() << " " << E.nor3Y() << " " << E.nor3Z() << endl; 
+		cout << endl;
+		cout << "3D vector dot product:" << endl;
 
-		C = A - B; //C will equal ((A.x - B.x)(A.y - B.y))
-		C.print2(); //print C sub value
-		cout << endl; //new line
-		cout << "2D vector magnitude:" << endl; //info
-		cout << "Magnitude of A is: " << A.mag2() << endl; //info and prints out magnitude of vector A
-		cout << "Magnitude of B is: " << B.mag2() << endl; //info and prints out magnitude of vector B
-		cout << endl; //new line
-
-		cout << "2D vector normalise:" << endl; //info
-		cout << "Normalise vector A is: " << A.nor2X() << " " << A.nor2Y() << endl; //info and prints out normalise x and y of vector A
-		cout << "Normalise vector B is: " << B.nor2X() << " " << B.nor2Y() << endl; //info and prints out normalise x and y of vector B
-		cout << endl; //new line
-		cout << "2D vector dot product:" << endl; //info
-
-		C = A * B; //C will equal ((A.x * B.x)(A.y * B.y)) 
-		C.dot2(); //This will print out the dot product of C
-		cout << endl; //new line
-
-		
-		cout << "3D vector D and E:" << endl; //info
-		D.x = -7, D.y = 9, D.z = 2; //D = (-7,9,2)
-		E.x = 3, E.y = 14, E.z = -3; //E = (3,14,-3)
-		D.print3(); //print test
-		E.print3(); //print test
-		cout << endl; //new line
-		cout << "3D vector addition:" << endl; //info
-
-		F = D + E; //F will equal ((D.x + E.x)(D.y + E.y)(D.z + E.z))
-		F.print3(); //print F add value
-		cout << endl; //new line
-		cout << "3D vector subtraction:" << endl; //info
-
-		F = D - E; //F will equal ((D.x - E.x)(D.y - E.y)(D.z - E.z))
-		F.print3(); //print F sub value
-		cout << endl; //new line
-		cout << "3D vector magnitude:" << endl; //info
-		cout << "Magnitude of D is: " << D.mag3() << endl; //info and prints out magnitude of vector D
-		cout << "Magnitude of E is: " << E.mag3() << endl; //info and prints out magnitude of vector E
-		cout << endl; //new line
-		cout << "3D vector normalise:" << endl; //info
-		cout << "Normalise vector D is: " << D.nor3X() << " " << D.nor3Y() << " " << D.nor3Z() << endl; //info and prints out normalise x, y and z of vector D
-		cout << "Normalise vector E is: " << E.nor3X() << " " << E.nor3Y() << " " << E.nor3Z() << endl; //info and prints out normalise x, y and z of vector E
-		cout << endl; //new line
-		cout << "3D vector dot product:" << endl; //info
-
-		F = D * E; //F will equal ((D.x * E.x)(D.y * E.y)(D.z * E.z)) 
-		F.dot3(); //This will print out the dot product of F
-		cout << endl; //new line
+		F = D * E; 
+		F.dot3(); 
+		cout << endl; 
 
 		cout << "3D vector cross product: " << endl;
 		F = D / E;
